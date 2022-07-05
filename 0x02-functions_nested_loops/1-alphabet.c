c0cd58997010dc200fb88eccb1bab7fdf6972fdd
Oluwatobi Amoniyan <102077396+Oluwatoby8965@users.noreply.github.com>
2022-07-05 18:01:28 +0100

    alpha

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,18 +1,16 @@
 #include "main.h"
 
 /**
- * print_alphabet - prints lower case alphabet
- *
- **/
+ * print_alphabet -> prints the lowercase alphabets
+ */
 
-void print_alphabet(void)
+void print_alphabet(void);
 {
+    int j;
 
-	char i;
-
-	for (i = 'a'; i <= 'z'; i++)
-	{
-		_putchar(i);
-	}
-	_putchar('\n');
+    for (j = 'a'; j <= 'z'; j++)
+    {
+        _putchar(j);
+    }
+    putchar('\n');
 }
