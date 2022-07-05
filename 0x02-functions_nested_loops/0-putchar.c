bc0063bf0520e5cb4ebcf4482f113fb946d950b9
Oluwatobi Amoniyan <102077396+Oluwatoby8965@users.noreply.github.com>
2022-07-05 17:43:37 +0100

    Now

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * main - Prints putchar
- * Return: always 0
- */
-int main(void)
+* main - prints putchar
+* Return: always 0
+*/
+int main (void) 
 {
 	_putchar('_');
 	_putchar('p');
