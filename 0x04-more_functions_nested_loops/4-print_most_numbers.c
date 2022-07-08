854b84333f004477bc6da0705e709ae548d97da8
Oluwatoby8965 <amoniyano1@gmail.com>
2022-07-08 06:30:10 -0700

    jhvjh

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -7,14 +7,11 @@
 */
 void print_most_numbers(void)
 {
-	char i;
-	
+	int i;
+
 	for (i = 48; i <= 57; i++)
-	{
 		if ((i != 50) && (i != 52))
 			_putchar(i);
-	}
+
 	_putchar('\n');
-	
-	return(0);
 }
