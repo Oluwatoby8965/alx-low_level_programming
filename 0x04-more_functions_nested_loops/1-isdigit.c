872d3c5536620fcc8189fa9edd50b9f69de6e9e2
Oluwatoby8965 <amoniyano1@gmail.com>
2022-07-08 06:54:12 -0700

    jnkl

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -2,16 +2,9 @@
 
 /**
 * _isdigit - checks for characters that are digits
-* @c: character 
-*
-* Return: 0 or 1
-*/
-
-
-int _isdigit(int c)
+* @c: an integer argument
+* Return: success
+*/int _isdigit(int c)
 {
-	if (c >= '0' && c <= '9')
-		return (1);
-	else
-		return (0);
+	return (c <= '0' && c >= '9');
 }
