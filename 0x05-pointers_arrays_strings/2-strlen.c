91f571688437b500f682def64dce1486e80ee179
Oluwatoby8965 <amoniyano1@gmail.com>
2022-07-12 17:52:02 -0700

    new array commit

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,11 +1,23 @@
 #include "main.h"
-#include <string.h>
+
 /**
-* _strlen -> function to get the length of a string
-* @: string pointer to passed this function
-* Return: returns length of the string
+* _strlen - print string lingth
+* @s: string will be calculated
+*
+* Return: length of string
 */
 int _strlen(char *s)
 {
-	return (strlen(s));
+	int len, i;
+
+	len = 0;
+	i = 0;
+
+	while (s[i] != '\0')
+	{
+		len++;
+		i++;
+	}
+
+	return (len);
 }
