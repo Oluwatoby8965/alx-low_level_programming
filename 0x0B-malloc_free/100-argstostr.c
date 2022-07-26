f25f60f4b06d4848ebb96f203413f8e84ab12cd1
Oluwatobi Amoniyan <102077396+Oluwatoby8965@users.noreply.github.com>
2022-07-26 11:53:11 +0100

    Advance hot fix

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
- * argstostr - concatenates all the arguments of a program.
- * @ac: argument count.
- * @av: argument vector.
- *
- * Return: pointer of an array of char
- */
+* argstostr - concatenates all the arguments of a program.
+* @ac: argument count.
+* @av: argument vector.
+*
+* Return: pointer of an array of char
+*/
 char *argstostr(int ac, char **av)
 {
 	char *aout;
--- 0x0B-malloc_free/101-main.c
+++ 0x0B-malloc_free/101-main.c
@@ -3,11 +3,11 @@
 #include <stdlib.h>
 
 /**
- * print_tab - Prints an array of string
- * @tab: The array to print
- *
- * Return: nothing
- */
+* print_tab - Prints an array of string
+* @tab: The array to print
+*
+* Return: nothing
+*/
 void print_tab(char **tab)
 {
     int i;
@@ -19,10 +19,10 @@ void print_tab(char **tab)
 }
 
 /**
- * main - check the code for ALX School students.
- *
- * Return: 1 if an error occurred, 0 otherwise
- */
+* main - check the code for ALX School students.
+*
+* Return: 1 if an error occurred, 0 otherwise
+*/
 int main(void)
 {
     char **tab;
